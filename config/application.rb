01118bbad679ab9c3a230cb5edf0b8852b85efad
rafaelsantossgaldino <rafaelsantossgaldino2@gmail.com>
2022-08-01 09:14:09 -0300

    add i18n

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,9 @@ module Rblog
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
+    
+    config.i18n.default_locale = "pt_br"
+    config.time_zone = "Brasilia"
 
     # Configuration for the application, engines, and railties goes here.
     #
