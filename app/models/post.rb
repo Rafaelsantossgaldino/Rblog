def7889ea101491f642eecb2bed4e130f4765044
rafaelsantossgaldino <rafaelsantossgaldino2@gmail.com>
2022-08-16 13:18:47 -0300

    add friendly e ogsearch

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,14 @@
 class Post < ApplicationRecord
+  include PgSearch
+  extend FriendlyId
+
   validates :title, :author, :body, presence: true
 
   has_many :comments
+
+  friendly_id :title, use: :slugged
+
+  pg_search_scope :search, 
+  agaist: %i[title author body],
+  associated_agaist: { comments: %i[body] }
 end
